73d07846a579304fa39d123b4cdc95b30cc09b61
Brianna Tanusi <btanusi@btanusi.lan>
2020-11-19 14:25:49 -0800

    added NOT NULL to item_id

--- 03_pantrylist.sql
+++ 03_pantrylist.sql
@@ -1,7 +1,13 @@
 -- Pantry List
 CREATE TABLE tbl_pantrylist (
 	pantry_item_id serial PRIMARY KEY,
-	item_id int REFERENCES tbl_items ON DELETE CASCADE,
+	item_id int NOT NULL REFERENCES tbl_items ON DELETE CASCADE,
 	expiration date,
 	amount int
 );
+
+-- Insert Pantry Items
+INSERT INTO tbl_pantrylist (item_id) VALUES
+    (1),
+    (8)
+;
