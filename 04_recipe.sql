78eee875ae8ecac27113e0af4e02f07d10328507
azteckind <73482202+azteckind@users.noreply.github.com>
2020-11-19 11:17:07 -0800

    tables created; might need some polishing later

--- 04_recipe.sql
+++ 04_recipe.sql
@@ -1,6 +1,21 @@
 -- Recipe List
 CREATE TABLE tbl_recipes (
-  recipe_id serial PRIMARY KEY,
+  recipe_id INT serial PRIMARY KEY,
   recipe_name text NOT NULL,
   food_group_id INT
+);
+
+-- Food Group List
+CREATE TABLE tbl_food_group (
+    food_group_id INT serial PRIMARY KEY,
+    food_group_name text
+);
+
+-- Quantity List
+CREATE TABLE tbl_quantity (
+    quantity_id INT serial PRIMARY KEY,
+    recipe_id INT,
+    ingredient_id INT,
+    ingredient_measurement_id INT,
+    ingredient_quantity FLOAT
 );
