13ba671ebccfefce9b31dba4ca6c7b6305da7bd1
fallensteed <scott@hubertart.com>
2020-11-19 10:42:22 -0700

    added shoppinglist

---
+++ shoppinglist.sql
@@ -0,0 +1,14 @@
+-- Shopping List Categories
+CREATE TABLE tbl_shoppinglist (
+  list_id serial PRIMARY KEY,
+  list_name text NOT NULL,
+  list_created date,
+  list_updated date
+);
+
+-- Shopping List Items
+CREATE TABLE tbl_shoppinglist_items (
+  link_id serial PRIMARY KEY,
+  list_id int REFERENCES tbl_shoppinglist ON DELETE CASCADE,
+  item_id int REFERENCES tbl_items ON DELETE CASCADE,
+);
