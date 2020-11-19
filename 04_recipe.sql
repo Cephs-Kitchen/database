974ff13fc76d656bf9aeb870483d7d3e0739cd44
azteckind <73482202+azteckind@users.noreply.github.com>
2020-11-19 11:22:57 -0800

    fixed the primary keys

--- 04_recipe.sql
+++ 04_recipe.sql
@@ -1,19 +1,19 @@
 -- Recipe List
 CREATE TABLE tbl_recipes (
-  recipe_id INT serial PRIMARY KEY,
+  recipe_id serial PRIMARY KEY,
   recipe_name text NOT NULL,
   food_group_id INT
 );
 
 -- Food Group List
 CREATE TABLE tbl_food_group (
-    food_group_id INT serial PRIMARY KEY,
+    food_group_id serial PRIMARY KEY,
     food_group_name text
 );
 
 -- Quantity List
 CREATE TABLE tbl_quantity (
-    quantity_id INT serial PRIMARY KEY,
+    quantity_id serial PRIMARY KEY,
     recipe_id INT,
     ingredient_id INT,
     ingredient_measurement_id INT,
